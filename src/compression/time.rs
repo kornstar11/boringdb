35d95f546450b407aabe39d396e379cd64cb0d4a
Ben Kornmeier <benkornmeier@gmail.com>
2024-04-29 21:45:54 -0600

    fix writer

--- src/compression/time.rs
+++ src/compression/time.rs
@@ -35,23 +35,21 @@ impl BitWriter {
     }
 
     pub fn write(&mut self, mut to_write: u64, mut bits_to_write: usize) {
-        let new_offset = self.offset + bits_to_write;
         // mask off to_write
         //println!("to_write1 {to_write:b}");
         let mask = !(u64::MAX << bits_to_write);
         to_write = mask & to_write;
-        if new_offset > U64_BITS as _ {
+        if self.offset + bits_to_write > U64_BITS as _ {
             let remaining = U64_BITS - self.offset; // 64 - 62 = 2
-            let shift_right_pos = bits_to_write - remaining; //7 - 2 = 5
-            let shifted = to_write.rotate_right(shift_right_pos as u32); // 1, 1, 1,  0, 0, 0,  0
-                                                                       //                 1,  1
-            
-            self.scratch |= (shifted & (u64::MAX >> shift_right_pos));
+            // write what we can into the scratch
+            let remaining_after_write = bits_to_write - remaining; //7 - 2 = 5
+            let masked_current = to_write >> remaining;
+            self.scratch |= masked_current;
             self.flush();
 
-            to_write = shifted & (u64::MAX << remaining);
-            self.offset += shift_right_pos;
-            return;
+            bits_to_write = remaining_after_write;
+            to_write = !(u64::MAX >> (U64_BITS - bits_to_write));
+            
         }
         self.scratch |= to_write << (U64_BITS - self.offset);
         self.offset += bits_to_write;
@@ -99,16 +97,25 @@ impl BitReader {
     pub fn read(&mut self, mut bits_to_read: usize) -> u64 {
         let mut acc = 0;
         let remaining = U64_BITS - self.offset;
+        let s = self.scratch;
+        println!("1bits_to_read: {}\n1remaining: {}\n1offset: {}\nscr: {s:b}\n=======", bits_to_read, remaining, self.offset);
         if bits_to_read > remaining {
-            acc |= self.scratch & (u64::MAX >> self.offset);
-            acc <<= bits_to_read - remaining;
+            let s = self.scratch;
+            let mask = (u64::MAX << self.offset);
+            println!("Flipping:\n scr: {s:b}\n acc: {acc:b}\n mask: {mask:b}");
+            acc |= self.scratch & mask;
+            acc <<= remaining; //bits_to_read - remaining;
             self.rotate_scratch();
             bits_to_read -= remaining;
         }
         let mask = !(u64::MAX >> bits_to_read);
-        acc |= (self.scratch & mask) >> U64_BITS - bits_to_read + 1;
+        println!("mask: {mask:b}");
+        acc |= (self.scratch & mask) >> U64_BITS - bits_to_read;
+        let xx = self.scratch;
+        println!(" acc: {acc:b}\n scr: {xx:b}\n");
+        println!("bits: {}", bits_to_read);
         self.scratch <<= bits_to_read;
-        self.offset += 1;
+        self.offset += bits_to_read;
         return acc;
     }
 
@@ -139,136 +146,32 @@ mod test {
         assert_eq!(reader.read_bit(), true);
         assert_eq!(reader.read_bit(), false);
         assert_eq!(reader.read_bit(), true);
+        assert_eq!(reader.read_bit(), true);
+    }
+    #[test]
+    fn round_trip_128bits() {
+        let mut writer = BitWriter::default();
+        for i in 0..128 {
+            writer.on();
+            // if i % 2 == 0 {
+            //     writer.on();
+            // } else {
+            //     writer.off();
+            // }
+        }
+        let buf = writer.finish().freeze();
+        let v = buf.clone().get_u64();
+        println!("v: {v:b}");
+        let mut reader = BitReader::from(buf);
+        for i in 0..128 {
+            let bit = reader.read_bit();
+            println!("Bit {} at {}", bit, i);
+            // if i % 2 == 0 {
+            //     assert!(bit == true);
+            // } else {
+            //     assert!(bit == false);
+            // }
+
+        }
     }
 }
-
-// pub struct TimeCompressor {
-//     prev_value: i64,
-//     prev_delta: i64,
-//     scratch: u64,
-//     bits_used: u32,
-//     values_written: u64,
-//     buf: BytesMut,
-// }
-
-
-
-// impl TimeCompressor {
-//     pub fn new() -> Self {
-//         let mut me = Self {
-//             prev_value: 0,
-//             prev_delta: 0,
-//             scratch: 0,
-//             bits_used: 0,
-//             values_written: 0,
-//             buf: BytesMut::new(),
-//         };
-//         // we write a u64 0 so that when we finish we can encode the amount of values used
-//         me.buf.put_u64(0);
-//         me
-//     }
-//     fn encode_value(&mut self, i: i64) {
-//         //let leading = i.leading_zeros();
-        
-//         let (mask, mask_size, bits_to_write) = match i {
-//             0 => R0,
-//             i if -63 < i && i < 64 => R1,
-//             i if -255 < i && i < 256 => R2,
-//             i if -2047 < i && i < 2048 => R3,
-//             _ => R4,
-//         };
-//         let mut value = i as u64;
-//         let leading = value.leading_zeros();
-//         let value_shifts = leading - mask_size;
-//         //let value_bits_used = mask_size + (u64::BITS - leading);
-//         value <<= value_shifts;
-//         value |= mask;
-
-//         //write what we have ignoring the overflow
-//         self.scratch |= value >> self.bits_used;
-//         self.bits_used += bits_to_write;
-//         self.values_written += 1;
-
-//         // rotate the "scratch"
-//         if self.bits_used > u64::BITS {
-//             self.buf.put_u64(self.scratch);
-//             self.scratch = 0;
-//             //split
-//             let overflow = self.bits_used - u64::BITS;
-//             self.scratch |= value << overflow;
-//             self.bits_used = overflow;
-//         }
-//     }
-
-// }
-
-// impl Compressor<i64, Vec<u8>> for TimeCompressor {
-//     fn compress(&mut self, i: i64) -> Result<()> {
-//         let delta = self.prev_value - i;
-//         let dod = self.prev_delta - delta;
-//         self.prev_value = i;
-//         self.prev_delta = delta;
-//         self.encode_value(dod);
-//         Ok(())
-//     }
-
-//     fn finish(mut self) -> Result<Vec<u8>> {
-//         {
-//             let (mut first, _) = self.buf.split_at_mut(8);
-//             first.put_u64(self.values_written);
-//         }
-//         Ok(self.buf.freeze().into())
-//     }
-// }
-
-// ///
-// /// TimeDecompressor
-// pub struct TimeDecompressor {
-//     buf: BytesMut,
-//     scratch: u64,
-//     values_to_read: u64,
-//     bit_offset: i32,
-// }
-
-// impl From<BytesMut> for TimeDecompressor {
-//     fn from(mut buf: BytesMut) -> Self {
-//         let values_written = buf.get_u64();
-//         let scratch = buf.get_u64();
-//         Self {
-//             buf,
-//             scratch,
-//             values_to_read: values_written,
-//             bit_offset: u64::BITS as _,
-//         }
-//     }
-// }
-
-// impl Iterator for TimeDecompressor {
-//     type Item = i64;
-
-//     fn next(&mut self) -> Option<Self::Item> {
-//         self.values_to_read -= 1;
-//         if self.values_to_read == 0 {
-//             return None;
-//         }
-//         for (mask, mask_size, bits_to_read) in MASKS {
-//             let new_bit_pos = self.bit_offset - ((bits_to_read + mask_size) as i32);
-//             if new_bit_pos < 0 {
-//                 // get remainder from scratch
-//                 let shift_over = u64::BITS - self.bit_offset as u32;
-//                 let mut value = self.scratch << shift_over;
-//                 self.scratch = self.buf.get_u64();
-
-//                 let new_shift_over = new_bit_pos.abs() as u32;
-//                 value |= (self.scratch >> (u64::BITS - new_shift_over)) << u64::BITS - (shift_over + new_shift_over);
-//                 //discard the mask
-//             }
-//             let read_mask = self.scratch >> u64::BITS - mask_size;
-//             if read_mask != mask {
-//                 continue;
-//             }
-//         }
-//         todo!()
-//     }
-// }
-
